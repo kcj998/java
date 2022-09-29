cd4ae86c80b7eca709da84b16fd416b5145ca3b3
kcj998 <kcj998loveyukikaze@gmail.com>
2022-09-29 16:15:08 +0800

    3333

---
+++ JAVA课上/Introduce.java
@@ -0,0 +1,16 @@
+package JAVA课上;
+
+public class Introduce {
+    public static void main(String[] args) {
+    String name = "魏子健";
+    int age = 18;
+    double stature = 177;
+    double weight = 75.5;
+    char sex = '男';
+    String interest = "计算机编程";
+    System.out.println("姓名：" + name);
+    System.out.println("年龄：" + age);
+    System.out.println("身高：" + stature);System.out.println("体重:" + weight);
+    System.out.println("性别：" + sex);
+    System.out.println("兴趣爱好：" + interest);}
+    }
