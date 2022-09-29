5f3c083cd58c135b51e763f3db9187440059d164
kcj998 <kcj998loveyukikaze@gmail.com>
2022-09-29 16:16:22 +0800

    2-3

---
+++ JAVA课上/Example2_3.java
@@ -0,0 +1,9 @@
+package JAVA课上;
+
+import java.util.Scanner;
+public class Example2_3 {
+public static void main(String[] args) {System.out.println("请输入两个整型数据：");Scanner reader = new Scanner(System.in);int a = reader.nextInt();
+int b = reader.nextInt();
+int sum = a + b;
+System.out.println(a + "+" + b + "="+sum);}
+}
