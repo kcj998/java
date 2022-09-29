205ae6f2e57e488a9c951cf867d824f061ba4353
kcj998 <kcj998loveyukikaze@gmail.com>
2022-09-29 16:06:39 +0800

    1st

---
+++ JAVA课上/Example2_1.java
@@ -0,0 +1,15 @@
+package JAVA课上;
+
+public class Example2_1 {
+    public static void main(String[] args) 
+    {
+    System.out.println("byte型数据的字节数："+Byte.SIZE);
+    System.out.println("byte型数据的取值范围："+Byte.MIN_VALUE+"~"+Byte.MAX_VALUE);
+    System.out.println("short型数据的字节数："+Short.SIZE);
+    System.out.println("short型数据的取值范围："+Short.MIN_VALUE+"~"+Short.MAX_VALUE);
+    System.out.println("int型数据的字节数："+Integer.SIZE);
+    System.out.println("int型数据的取值范围："+Integer.MIN_VALUE+"~"+Integer.MAX_VALUE);
+    System.out.println("long型数据的字节数："+Long.SIZE);
+    System.out.println("long型数据的取值范围："+Long.MIN_VALUE+"~"+Long.MAX_VALUE);
+    }
+}
