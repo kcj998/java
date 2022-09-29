bc6a2167f211f3ec203151ddaa050d165af86e1e
kcj998 <kcj998loveyukikaze@gmail.com>
2022-09-29 16:11:30 +0800

    2nd

---
+++ JAVA课上/Example2_2.java
@@ -0,0 +1,13 @@
+package JAVA课上;
+
+public class Example2_2 {
+    public static void main(String[] args)
+    {
+    int x = '魏';
+    int y = '子';
+    int z = '健';
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    }
+}
